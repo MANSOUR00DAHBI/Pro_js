59ef692aa4301899766ed79dd2c639fd608f87f8
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-09 00:19:58 +0100

    move circle in page

--- index.js
+++ index.js
@@ -10,18 +10,35 @@ function fillcircle(context, x,y,radius,color = "green"){
 (() => {
     
     console.log("Hello , World !");
-    
-
     const canvas = document.getElementById("game");
     const context = canvas.getContext("2d");
+    const radius  = 69;
     const Width = canvas.width ;
     const Height = canvas.height;
-    const radius  = 69;
-    fillcircle(context,Width / 2, Height / 2,radius,"red");
+    
+    let start ;
+    let x = Width / 2;
+    let y = Height / 2;
+    let dx = 100;
+    let dy = 100;
+    function step(timestamp){
+        if(start === undefined){
+            start = timestamp;
+        }
+        const dt = (timestamp - start) * 0.001;
+        start = timestamp;
+        if(x + radius   >= Width || x - radius <= 0 ) dx =-dx ;
+        if(y + radius  >= Height || y - radius <= 0 ) dy =-dy ;
+        x += dx * dt ;
+        y += dy * dt ;
+        context.clearRect(0,0,Width,Height);
+        fillcircle(context,x, y,radius,"red");
+        window.requestAnimationFrame(step);
+    }
+     window.requestAnimationFrame(step);
+
    // context.moveTo(0,0);
    // context.lineTo(Width,Height );
    // context.stroke();
-
-
 
 })();
