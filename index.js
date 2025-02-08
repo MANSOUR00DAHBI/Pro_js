05c0c4b6b57ec6d8ca8e5d96eee1637803c51d6c
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-08 16:56:59 +0100

    Circle

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+// INDEX.JS FILE 
+
+function fillcircle(context, x,y,radius,color = "green"){
+    context.beginPath();
+    context.arc(x,y,radius,0,2*Math.PI ,false);
+    context.fillStyle = color ;
+    context.fill();
+}
+
 (() => {
+    
     console.log("Hello , World !");
-    let canvas = document.getElementById("game");
-    let context = canvas.getContext("2d");
-    context.moveTo(0,0);
-    context.lineTo(200,100);
-    context.stroke();
+    
+
+    const canvas = document.getElementById("game");
+    const context = canvas.getContext("2d");
+    const Width = canvas.width ;
+    const Height = canvas.height;
+    const radius  = 69;
+    fillcircle(context,Width / 2, Height / 2,radius,"red");
+   // context.moveTo(0,0);
+   // context.lineTo(Width,Height );
+   // context.stroke();
+
+
+
 })();
