1a0f511637fa3d9e657d4db9a48a5ebcb7afb4cd
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-12 00:35:43 +0100

    move With Key

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 // INDEX.JS FILE 
 
-
-/*
-function V2(x,y){
-    return {x,y};
-}
-
-function addV2(a,b){
-    return V2(a.x  + b.x , a.y + b.y);
-}
-
-function scaleV2(v , s ){
-    return V2(v.x * s , v.y * s );
-}
-*/
 class V2 {
     constructor (x,y){
         this.x = x ;
@@ -38,11 +24,10 @@ function fillcircle(context,center,radius,color = "green"){
 
 (() => {
     
-    //console.log("Hello , World !");
     const canvas = document.getElementById("game");
     const radius  = 69;
     const context = canvas.getContext("2d");
-    const speed = 100;
+    const speed = 1000;
     let start ;
     let pos = new V2(radius + 10 , radius + 10);
     let lev  = new V2( 0 , 0 );
@@ -51,25 +36,20 @@ function fillcircle(context,center,radius,color = "green"){
              'KeyS'  : new V2( 0 , speed ),              
              'KeyW' : new V2(0 , -speed ),          
              'KeyA'  : new V2( -speed, 0 ),             
-             'KeyD'  : new V2( speed , 0 ),    }
-    /*let dy = speed ;
-    let dx = speed ;*/
+             'KeyD'  : new V2( speed , 0 ), 
+    };
+
     function step(timestamp){
         if(start === undefined){
             start = timestamp;
         }
-        const dt = (timestamp - start) * 0.001;
-        start = timestamp;
+    const dt = (timestamp - start) * 0.001;
+    start = timestamp;
 
     const Width =   window.innerWidth ;
     const    Height =  window.innerHeight;
     canvas.width = Width;
     canvas.height = Height;
-
-       /* if(x + radius   >= Width || x - radius <= 0 ) dx =-dx ;
-        if(y + radius  >= Height || y - radius <= 0 ) dy =-dy ;
-        x += dx * dt ;
-        y += dy * dt ;*/
         pos = pos.add(lev.scale(dt));
         context.clearRect(0,0,Width,Height);
         fillcircle(context,pos,radius,"red");
@@ -81,20 +61,12 @@ function fillcircle(context,center,radius,color = "green"){
           if(event.code in directionMap){
               lev = directionMap[event.code];
           }   
-     /* switch (event.code){
-             case 'KeyS' : {
-                 console.log("down");
-             } break;
-             case 'KeyW' : {
-                 console.log("up");
-             } break;
-             case 'KeyA' : {
-                 console.log("left");
-             } break;
-             case 'KeyD' : {
-                 console.log("right");
-             } break;
-         }*/
+     });
+
+          document.addEventListener('keyup' , event => {
+          if(event.code in directionMap){
+              lev = new V2(0,0);
+          }   
      });
 
 })();
